9bf61391e4141efd158df25e7e1fe8dbd0f965ac
Allen Thomas <allen.thomas.dev@gmail.com>
2024-04-18 19:13:16 +0530

    new colorscheme catppuccin

--- init.lua
+++ init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+--purple :as in clojure, need to move it to colorscheme.lua
+vim.opt.termguicolors = true
+vim.cmd.colorscheme("catppuccin-frappe")
+--vim.cmd("call darcula#Hi('@string.special.symbol.clojure', [ '#9876aa', 255], darcula#palette.null, 'italic')")
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+  pattern = { "conjure-log-*" },
+  callback = function(args)
+    local diagnostics = args.data.diagnostics
+
+    if diagnostics[1] ~= nil then
+      local bufnr = diagnostics[1]["bufnr"]
+      local namespace = diagnostics[1]["namespace"]
+      vim.diagnostic.disable(bufnr)
+      vim.diagnostic.reset(namespace, bufnr)
+    end
+  end,
+})
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,15 +5,14 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
---purple :keywords in clojure
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    --  { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -27,7 +26,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
